e24254dd7c95a2d1b84619a42c368a3144720f1a
adeavenged <dheendra7393@gmail.com>
2018-08-07 18:10:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sertifikasi SKKNI</title>
+</head>
+<body>
+	<!-- 
+		nama, nik, hp, email, skema sertifikasi, tempat uji kompetentsi, rekomendasi, tanggal terbit sertifikat, tanggal lahir, organisasi
+	-->
+	<table>
+		<tr>
+			<td></td>
+		</tr>
+	</table>
+
+</body>
+</html>
