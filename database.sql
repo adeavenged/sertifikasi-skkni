d7fdc2b3e09932b89bba265d723f7880d2e87c26
adeavenged <dheendra7393@gmail.com>
2018-08-08 09:52:02 +0700

    version 3.0

--- database.sql
+++ database.sql
@@ -11,41 +11,18 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table db_sertifikat_skkni.tb_hasil_sertifikasi
-CREATE TABLE IF NOT EXISTS `tb_hasil_sertifikasi` (
-  `id_trans` int(5) NOT NULL AUTO_INCREMENT,
-  `assor` varchar(50) NOT NULL DEFAULT '0',
-  `tgl_terbit_sk` date DEFAULT NULL,
-  `status_rekomendasi` enum('KOMPETEN','BELUM KOMPETEN') NOT NULL,
-  PRIMARY KEY (`id_trans`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table db_sertifikat_skkni.tb_hasil_sertifikasi: ~0 rows (approximately)
-DELETE FROM `tb_hasil_sertifikasi`;
-/*!40000 ALTER TABLE `tb_hasil_sertifikasi` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_hasil_sertifikasi` ENABLE KEYS */;
-
 -- Dumping structure for table db_sertifikat_skkni.tb_lokasi
 CREATE TABLE IF NOT EXISTS `tb_lokasi` (
   `id_lok` int(11) NOT NULL AUTO_INCREMENT,
   `tuk` varchar(50) NOT NULL,
   PRIMARY KEY (`id_lok`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_sertifikat_skkni.tb_lokasi: ~10 rows (approximately)
+-- Dumping data for table db_sertifikat_skkni.tb_lokasi: ~1 rows (approximately)
 DELETE FROM `tb_lokasi`;
 /*!40000 ALTER TABLE `tb_lokasi` DISABLE KEYS */;
 INSERT INTO `tb_lokasi` (`id_lok`, `tuk`) VALUES
-	(11, 'LOKASI 1'),
-	(12, 'LOKASI 2'),
-	(13, 'LOKASI 3'),
-	(14, 'LOKASI 4'),
-	(15, 'LOKASI 5'),
-	(16, 'LOKASI 6'),
-	(17, 'LOKASI 7'),
-	(18, 'LOKASI 8'),
-	(19, 'LOKASI 9'),
-	(20, 'LOKASI 10');
+	(31, 'Kota Jambi');
 /*!40000 ALTER TABLE `tb_lokasi` ENABLE KEYS */;
 
 -- Dumping structure for table db_sertifikat_skkni.tb_peserta
@@ -60,18 +37,20 @@ CREATE TABLE IF NOT EXISTS `tb_peserta` (
   `email` varchar(100) NOT NULL,
   `organisasi` text NOT NULL,
   `rekomendasi` enum('BERKOMPETENSI','BELUM BERKOMPETENSI') DEFAULT NULL,
+  `tgl_terbit` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_tb_peserta_tb_skema` (`id_skema`),
   KEY `FK_tb_peserta_tb_lokasi` (`id_lokasi`),
   CONSTRAINT `FK_tb_peserta_tb_lokasi` FOREIGN KEY (`id_lokasi`) REFERENCES `tb_lokasi` (`id_lok`),
   CONSTRAINT `FK_tb_peserta_tb_skema` FOREIGN KEY (`id_skema`) REFERENCES `tb_skema` (`id_skema`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table db_sertifikat_skkni.tb_peserta: ~0 rows (approximately)
 DELETE FROM `tb_peserta`;
 /*!40000 ALTER TABLE `tb_peserta` DISABLE KEYS */;
-INSERT INTO `tb_peserta` (`id`, `nik`, `id_skema`, `id_lokasi`, `nama`, `tgl_lahir`, `hp`, `email`, `organisasi`, `rekomendasi`) VALUES
-	(2, '1', 1, 11, 'PESERTA 1', '1993-07-07', '085227281672', 'peserta1@gmail.com', 'KAMPUS 1', 'BERKOMPETENSI');
+INSERT INTO `tb_peserta` (`id`, `nik`, `id_skema`, `id_lokasi`, `nama`, `tgl_lahir`, `hp`, `email`, `organisasi`, `rekomendasi`, `tgl_terbit`) VALUES
+	(12, '1509120703930001', 1, 31, 'ADE INDRA SAPUTRA', '1993-07-03', '628522728167', 'dheendra7393@gmail.com', 'STIKOM Dinamika Bangsa Jambi', 'BERKOMPETENSI', '2018-08-09'),
+	(13, '1571072702960021', 1, 31, 'FERI PEBRIANTO', '1996-02-27', '081272014721', 'feripebrianto22@gmail.com', 'STIKOM Dinamika Bangsa Jambi', 'BERKOMPETENSI', '2018-08-09');
 /*!40000 ALTER TABLE `tb_peserta` ENABLE KEYS */;
 
 -- Dumping structure for table db_sertifikat_skkni.tb_skema
@@ -80,22 +59,15 @@ CREATE TABLE IF NOT EXISTS `tb_skema` (
   `nama_skema` varchar(100) NOT NULL,
   `ket` text NOT NULL,
   PRIMARY KEY (`id_skema`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_sertifikat_skkni.tb_skema: ~10 rows (approximately)
+-- Dumping data for table db_sertifikat_skkni.tb_skema: ~3 rows (approximately)
 DELETE FROM `tb_skema`;
 /*!40000 ALTER TABLE `tb_skema` DISABLE KEYS */;
 INSERT INTO `tb_skema` (`id_skema`, `nama_skema`, `ket`) VALUES
-	(1, 'SKEMA 1', '-'),
-	(2, 'SKEMA 2', '-'),
-	(3, 'SKEMA 3', '-'),
-	(4, 'SKEMA 4', '-'),
-	(5, 'SKEMA 5', '-'),
-	(6, 'SKEMA 6', '-'),
-	(7, 'SKEMA 7', '-'),
-	(8, 'SKEMA 8', '-'),
-	(9, 'SKEMA 9', '-'),
-	(10, 'SKEMA 10', '-');
+	(1, 'Programmer Madya', '-'),
+	(11, 'Multimedia Madya', ''),
+	(12, 'Networking Madya', '');
 /*!40000 ALTER TABLE `tb_skema` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
