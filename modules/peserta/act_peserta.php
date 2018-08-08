8a8919f09db1a96f0017a9c0f0c6729069f6a4a5
adeavenged <dheendra7393@gmail.com>
2018-08-08 11:23:33 +0700

    version 3.0

--- modules/peserta/act_peserta.php
+++ modules/peserta/act_peserta.php
@@ -7,33 +7,40 @@
 
 
 	if ($mod == 'peserta' AND $act == 'simpan') {
-
-		mysql_query("INSERT INTO tb_peserta(id, 
-											nik, 
-											id_skema, 
-											id_lokasi, 
-											nama, 
-											tgl_lahir, 
-											hp, 
-											email, 
-											organisasi, 
-											rekomendasi,
-											tgl_terbit)
-									VALUES(NULL, 
-											'".$_POST['nik']."', 
-											'".$_POST['skema']."', 
-											'".$_POST['lokasi']."', 
-											'".$_POST['nama']."', 
-											'".$_POST['tgl_lahir']."', 
-											'".$_POST['hp']."', 
-											'".$_POST['email']."', 
-											'".$_POST['organisasi']."', 
-											'".$_POST['rekomendasi']."', 
-											'".$_POST['tgl_terbit']."')") or die(mysql_error());
-		echo"<script>
-			alert('Berhasil melakukan input data peserta...');
-			window.location=('../../index.php')
-		</script>";
+		if (!is_numeric($_POST['hp'])) {
+			echo"<script>
+				alert('Nomor HP harus angka...');
+				window.history.back()
+			</script>";
+		} else {
+			mysql_query("INSERT INTO tb_peserta(id, 
+												nik, 
+												id_skema, 
+												id_lokasi, 
+												nama, 
+												tgl_lahir, 
+												hp, 
+												email, 
+												organisasi, 
+												rekomendasi,
+												tgl_terbit)
+										VALUES(NULL, 
+												'".$_POST['nik']."', 
+												'".$_POST['skema']."', 
+												'".$_POST['lokasi']."', 
+												'".$_POST['nama']."', 
+												'".$_POST['tgl_lahir']."', 
+												'".$_POST['hp']."', 
+												'".$_POST['email']."', 
+												'".$_POST['organisasi']."', 
+												'".$_POST['rekomendasi']."', 
+												'".$_POST['tgl_terbit']."')") or die(mysql_error());
+			echo"<script>
+				alert('Berhasil melakukan input data peserta...');
+				window.location=('../../index.php')
+			</script>";
+		}
+			
 	} elseif ($mod == 'peserta' AND $act == 'edit') {
 		mysql_query("UPDATE tb_peserta SET nik = '".$_POST['nik']."', 
 											id_skema = '".$_POST['skema']."', 
